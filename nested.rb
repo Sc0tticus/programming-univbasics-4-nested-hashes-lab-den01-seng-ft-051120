f77cfedbfb7008f3ed6457de174d87cc3719d105
Scott Singer <ssinger303@gmail.com>
2020-05-02 01:18:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,7 +108,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_key][:known_for]
+programmer_hash[:alan_key][:known_for]="GUI"
+programmer_hash
 end
 
 def adding_to_dennis
