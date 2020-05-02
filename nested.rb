7e37152d576fb62e6f1fd830b988c5de8697c57f
Scott Singer <ssinger303@gmail.com>
2020-05-02 00:38:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,8 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:known_for][:languages]
-binding.pry
+programmer_hash[:dennis_ritchies][:known_for][:languages]
 end
 
 def adding_matz
