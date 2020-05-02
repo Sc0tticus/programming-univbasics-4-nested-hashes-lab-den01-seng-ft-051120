253c6bd915d72bcb8b26f01ebf672f593e5982a4
Scott Singer <ssinger303@gmail.com>
2020-05-02 00:39:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,7 +63,7 @@ def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]x
+#   :languages => ["LISP", "C"]
 # }
 # return the entire updated hash
 
